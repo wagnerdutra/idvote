beca07b6de0fc0150358c4a22ddeb9eccddfbab0
wagner.azevedo <wagner.azevedo@idwall.co>
2019-11-22 15:05:55 -0300

    fi

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
+          <link rel="stylesheet" href="https://idwall.co/css/font-margem.css"/>
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
         </Head>
 
@@ -21,6 +22,17 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
+
+        <style jsx>{`
+          html {
+            width: 100%;
+            height: 100%;
+          }
+          body {
+            font-family: 'Margem';
+          }
+        `}
+        </style>
       </html>
     )
   }
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import React from "react";
 
 const Home = () => (
   <section>
-    <div class="jumbotron">
-      <h1 class="display-4">Vote no seu idlabs preferido!</h1>
+    <div className="poll-title">
+      <h1>
+        Vote <br/> no seu idlabs preferido!
+      </h1>
     </div>
 
-    <div className="options">
+    <div className="poll-options">
       {['Time 1', 'Time 2', 'Time 3', 'Time 4'].map(id =>
         (
           <label htmlFor={id} key={id}>
@@ -15,30 +17,48 @@ const Home = () => (
           </label>
         )
       )}
-      <style jsx>{`
-        section {
-          display: flex;
-          flex-direction: center;
-          align-items: center;
-        }
-
-        .options {
-          width:100%;
-          margin:0;
-        }
-
-        .options label {
-          width:100%;
-          display:block;
-          background:#f9f9f9;
-          margin:0;
-        }
-
-        .options label:nth-child(odd) {
-          background:#f5f5f5;
-        }
-      `}</style>
     </div>
+
+    <style jsx>{`
+      section {
+        display: flex;
+        flex-direction: column;
+      }
+
+      .poll-title {
+        margin:0;
+        padding: 2rem;
+        display:block;
+      }
+
+      .poll-title h1 {
+        color:#5500ff;
+        font-size:2rem !important;
+        font-weight:bold !important;
+      }
+
+      .poll-options {
+        width:100%;
+        margin:0;
+        display:block;
+      }
+
+      .poll-options label {
+        width:100%;
+        display:block;
+        background:#f9f9f9;
+        margin:0;
+        padding: 8px;
+      }
+
+      .poll-options label:nth-child(odd) {
+        background:#f5f5f5;
+      }
+
+      .poll-options label input {
+        display: none;
+      }
+    `}</style>
   </section>
 );
 
